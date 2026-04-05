d2e400914eb04c362a0e50a17d3e9a3b7bbba337
ninetyninester <icaro.alencar99@gmail.com>
2026-04-05 20:08:54 -0300

    add a game break and a reset button

--- RockPaperScissors/rps1.js
+++ RockPaperScissors/rps1.js
@@ -9,24 +9,40 @@ let humanScore = 0;
 let computerScore = 0;
 
 function playRound(humanChoice, computerChoice) {
-    if (humanChoice == computerChoice) {
+    if (humanChoice == computerChoice) 
+    {
         return "Draw!<br>";
-    } else if (
-        (humanChoice == "rock" && computerChoice == "scissors") ||
-        (humanChoice == "paper" && computerChoice == "rock") ||
-        (humanChoice == "scissors" && computerChoice == "paper")
-    ) {
+    } 
+    else if (
+              (humanChoice == "rock" && computerChoice == "scissors") ||
+              (humanChoice == "paper" && computerChoice == "rock") ||
+              (humanChoice == "scissors" && computerChoice == "paper")
+            )
+    {
         humanScore++;
         return `You win! ${humanChoice} beats ${computerChoice}.<br>`;
-    } else {
+    }
+     else
+     {
         computerScore++;
         return `You lose! ${computerChoice} beats ${humanChoice}.<br>`;
-    }
+     }
 }
 
 function showScore (){
-    return `You: ${humanScore}<br>Computer: ${computerScore}`
+
+    if(humanScore>=5){
+    return `You: ${humanScore}<br>Computer: ${computerScore} <br> <br> Congratulation, you won the game!!!<br><br>Thanks for playing.`;
+    }
+    else if (computerScore>=5){
+    return `You: ${humanScore}<br>Computer: ${computerScore} <br> <br> Sorry, you lose...<br><br>Thanks for playing.`;
+    }
+    else return `You: ${humanScore}<br>Computer: ${computerScore}`;
 };
+
+function isGameOver(){
+    return humanScore >=5 || computerScore >=5;
+}
 
 // Display on page
 
@@ -36,13 +52,19 @@ document.body.appendChild(resultDiv);
 resultDiv.classList.add("result")
 
 document.querySelector(".rock").addEventListener("click", () => {
+    if(isGameOver()) return;
     resultDiv.innerHTML = `${playRound("rock", getComputerChoice())}<br>${showScore()}`;
 });
 
 document.querySelector(".paper").addEventListener("click", () => {
+    if(isGameOver()) return;
     resultDiv.innerHTML = `${playRound("paper", getComputerChoice())}<br>${showScore()}`;
 });
 
 document.querySelector(".scissors").addEventListener("click", () => {
+    if(isGameOver()) return;
     resultDiv.innerHTML = `${playRound("scissors", getComputerChoice())}<br>${showScore()}`;
 });
+
+document.querySelector(".reset").addEventListener("click", () => {
+    location.reload();});
